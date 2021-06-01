8fdc02200e567b7e0b323d3b12d585105a739ee2
Tunga Aruna <arunatunga.2919@gmail.com>
2021-06-01 15:37:37 +0530

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './components/user/user.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { CustomerComponent } from './components/customer/customer.component';
+import { MovieComponent } from './components/movie/movie.component';
+import { ShowComponent } from './components/show/show.component';
+import { TheatreComponent } from './components/theatre/theatre.component';
+import { BookingComponent } from './components/booking/booking.component';
+import { SeatComponent } from './components/seat/seat.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    AdminComponent,
+    CustomerComponent,
+    MovieComponent,
+    ShowComponent,
+    TheatreComponent,
+    BookingComponent,
+    SeatComponent
   ],
   imports: [
     BrowserModule,
